d5fb5468b8c13bdff8369171db8ed850b3166129
njmarko <marko.njegomir@uns.ac.rs>
2022-04-03 11:33:09 +0200

    Added initial spring setup.

---
+++ smart-home-backend/src/main/java/kiber/bezbednjaci/SecurehomeApplication.java
@@ -0,0 +1,13 @@
+package kiber.bezbednjaci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurehomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurehomeApplication.class, args);
+	}
+
+}
