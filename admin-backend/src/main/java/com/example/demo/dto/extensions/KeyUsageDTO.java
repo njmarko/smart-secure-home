401b0c39d972bdb5d474d1548ae3eaa09f83ecbc
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2022-04-17 23:58:26 +0200

    Reading certificate extension data.

--- admin-backend/src/main/java/com/example/demo/dto/extensions/KeyUsageDTO.java
+++ admin-backend/src/main/java/com/example/demo/dto/extensions/KeyUsageDTO.java
@@ -18,10 +18,10 @@ public class KeyUsageDTO extends ExtensionDTO {
 
 	public int toBitMask() {
 		Map<KeyUsageEnumDTO, Integer> hashMap = new HashMap<>();
-		hashMap.put(KeyUsageEnumDTO.KEY_ENCIPHERMENT, 32);
-		hashMap.put(KeyUsageEnumDTO.CRL_SIGN, 2);
-		hashMap.put(KeyUsageEnumDTO.DIGITAL_SIGNATURE, 128);
-		hashMap.put(KeyUsageEnumDTO.CERTIFICATE_SIGNING, 4);
+		hashMap.put(KeyUsageEnumDTO.KEY_ENCIPHERMENT, 32);		// 5
+		hashMap.put(KeyUsageEnumDTO.CRL_SIGN, 2);				// 1
+		hashMap.put(KeyUsageEnumDTO.DIGITAL_SIGNATURE, 128); 	// 7
+		hashMap.put(KeyUsageEnumDTO.CERTIFICATE_SIGNING, 4); 	// 2
 		return keyUsages.parallelStream()
 				.map(hashMap::get)
 				.map(x -> 1 << x)
