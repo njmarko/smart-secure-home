0f02ec1e314019ae94662619b5897a4a119ba299
Dusan Brkic <dusanbrk@gmail.com>
2022-04-18 04:34:47 +0200

    Update csrs-view.component.ts

--- frontend/src/app/pages/csrs-view/csrs-view.component.ts
+++ frontend/src/app/pages/csrs-view/csrs-view.component.ts
@@ -5,6 +5,7 @@ import { CertificateService } from 'src/app/services/certificate.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { ConfirmationService } from 'src/app/shared/confirmation-service/confirmation.service';
 import { ErrorService } from 'src/app/shared/error-service/error.service';
+import CertificatePurpose from 'src/app/model/certificates/enum/CerificatePurpose';
 
 @Component({
   selector: 'app-csrs-view',
@@ -47,6 +48,7 @@ export class CsrsViewComponent implements OnInit {
   fetchData(pageIdx: number, pageSize: number): void {
     this.waitingResults = true;
     this.certificateService.read(pageIdx, pageSize).subscribe((page) => {
+      this.csrs = page.content.map(element => {element.purpose = (CertificatePurpose as any)[element.purpose as string]; return element;})
       this.pageNum = page.pageable.pageNumber;
       this.pageSize = page.pageable.pageSize;
       this.totalPages = page.totalPages;
