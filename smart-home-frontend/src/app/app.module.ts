31170ca037bcc51a216f4d0ad6af32d67594a37b
Dusan Erdeljan <dusanerdeljan99@gmail.com>
2022-05-22 00:07:24 +0200

    Login page copy-pasta.

--- smart-home-frontend/src/app/app.module.ts
+++ smart-home-frontend/src/app/app.module.ts
@@ -4,8 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { LoginComponent } from './pages/login/login.component';
 import { MyObjectsComponent } from './pages/my-objects/my-objects.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { MyObjectsComponent } from './pages/my-objects/my-objects.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- smart-home-frontend/src/app/pages/login/login.component.ts
+++ smart-home-frontend/src/app/pages/login/login.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
-  constructor() { }
+  @Input() error!: string | null;
+  form: FormGroup;
 
-  ngOnInit(): void {
+  invalidUser: boolean = false;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private router: Router,
+    private snackBar: MatSnackBar,
+    private route: ActivatedRoute
+  ) {
+    this.form = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (!this.form.valid) {
+      return;
+    }
+    this.invalidUser = false;
+    // this.authService.login(this.form.value).subscribe({
+    //   next: response => {
+    //     this.currentUserService.setCurrentUser(response);
+    //     // TODO: Navigate to a page based on the user's role
+    //     // this.snackBar.open(`Welcome, ${response.name} ${response.surname}!`, "Dismiss", { duration: 5000, verticalPosition: "top" });
+    //     const destination: string | null = this.route.snapshot.queryParamMap.get('to');
+    //     if (destination) {
+    //       this.router.navigate([destination]);
+    //     } else {
+    //       this.router.navigate(["/employees"]);
+    //     }
+    //   },
+    //   error: _ => {
+    //     this.invalidUser = true;
+    //   }
+    // });
+    this.snackBar.open(`Welcome, ${this.form.value.email} ${this.form.value.password}!`, "Dismiss", { duration: 5000, verticalPosition: "top" });
   }
 
 }
