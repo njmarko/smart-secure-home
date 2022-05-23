dc52727e1eee44b371680ce1a70d48002b76b8d9
dusanbrkic <dusanbrk@gmail.com>
2022-05-23 23:39:04 +0200

    autorizacija za crud csr/srt

--- admin-backend/src/main/java/com/example/demo/controller/CertificatesController.java
+++ admin-backend/src/main/java/com/example/demo/controller/CertificatesController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -41,35 +42,41 @@ public class CertificatesController {
         this.csrCsrDTOEntityConverter = csrCsrDTOEntityConverter;
     }
 
+    @PreAuthorize("hasAuthority('READ_CERTIFICATES')")
     @GetMapping
         public Page<ReadCertificateResponse> read(@PageableDefault Pageable pageable) {
         var certificates = certificatesService.read(pageable);
         return certificates.map(toReadResponse::convert);
     }
 
+    @PreAuthorize("hasAuthority('SIGN_CSR')")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void saveCertificate(@RequestBody CsrSignDataDTO csrSignDataDTO) throws Exception {
         certificatesService.create(csrSignDataDTO);
     }
 
+    @PreAuthorize("hasAuthority('READ_CERTIFICATES')")
     @GetMapping(value = "/{serialNumber}")
     public ReadCertificateResponse read(@PathVariable Integer serialNumber) {
         var certificate = certificatesService.read(serialNumber);
         return toReadResponse.convert(certificate);
     }
 
+    @PreAuthorize("hasAuthority('READ_CERTIFICATES')")
     @GetMapping(value = "/{serialNumber}/validity")
     public CheckValidityResponse validity(@PathVariable Integer serialNumber) {
         var certificate = certificatesService.read(serialNumber);
         return toCheckValidityResponse.convert(certificate);
     }
 
+    @PreAuthorize("hasAuthority('READ_CERTIFICATES')")
     @GetMapping(value = "/{serialNumber}/status")
     public CertificateStatus getCertificateStatus(@PathVariable Integer serialNumber) {
         return certificatesService.readCertificateStatus(serialNumber);
     }
 
+    @PreAuthorize("hasAuthority('READ_CERTIFICATES')")
     @PostMapping(value = "/{serialNumber}/invalidate")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void invalidate(@PathVariable Integer serialNumber, @RequestBody InvalidateCertificateRequest request) {
@@ -96,17 +103,20 @@ public class CertificatesController {
         this.certificatesService.createCSR(pemCSR);
     }
 
+    @PreAuthorize("hasAuthority('READ_CSRS')")
     @GetMapping(value = "/csr")
     public Page<CsrDTO> getCertificateStatus(@PageableDefault Pageable pageable) {
         var page = certificatesService.readCsrData(pageable);
         return page.map(csrCsrDTOEntityConverter::convert);
     }
 
+    @PreAuthorize("hasAuthority('READ_CSRS')")
     @GetMapping(value = "/csr/{id}")
     public CsrDTO getCsr(@PathVariable(name = "id") Integer id) {
         return csrCsrDTOEntityConverter.convert(this.certificatesService.readCsr(id));
     }
 
+    @PreAuthorize("hasAuthority('READ_CSRS')")
     @DeleteMapping(value = "/csr/{id}")
     public void deleteCSR(@PathVariable(name = "id") Integer id) {
         this.certificatesService.deleteCsr(id);
