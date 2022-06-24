a97f38dd7250f8b35dce3dbfb6a84742014bb9df
Dusan Brkic <dusanbrk@gmail.com>
2022-06-24 11:00:31 +0200

    https done

--- smart-home-backend/src/main/java/kiber/bezbednjaci/config/BeanConfig.java
+++ smart-home-backend/src/main/java/kiber/bezbednjaci/config/BeanConfig.java
@@ -1,5 +1,9 @@
 package kiber.bezbednjaci.config;
 
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
@@ -7,7 +11,12 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.context.annotation.RequestScope;
 
+import javax.net.ssl.SSLContext;
 import javax.servlet.http.HttpServletRequest;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -23,8 +32,24 @@ public class BeanConfig {
     @Bean
     @RequestScope
     public RestTemplate restTemplate(RestTemplateSecurityInterceptor restTemplateSecurityInterceptor) {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setRequestFactory(new HttpComponentsClientHttpRequestFactory());
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+        SSLContext sslContext = null;
+        try {
+            sslContext = org.apache.http.ssl.SSLContexts.custom()
+                    .loadTrustMaterial(null, acceptingTrustStrategy)
+                    .build();
+        } catch (NoSuchAlgorithmException | KeyManagementException | KeyStoreException e) {
+            e.printStackTrace();
+        }
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setSSLSocketFactory(csf)
+                .build();
+        HttpComponentsClientHttpRequestFactory requestFactory =
+                new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        RestTemplate restTemplate = new RestTemplate(requestFactory);
+
         var interceptors = Objects.requireNonNullElse(restTemplate.getInterceptors(), new ArrayList<ClientHttpRequestInterceptor>());
         interceptors.add(restTemplateSecurityInterceptor);
         restTemplate.setInterceptors(interceptors);
--- smart-home-backend/src/main/java/kiber/bezbednjaci/controller/UserController.java
+++ smart-home-backend/src/main/java/kiber/bezbednjaci/controller/UserController.java
@@ -1,25 +1,17 @@
 package kiber.bezbednjaci.controller;
 
-import kiber.bezbednjaci.config.RestTemplateSecurityInterceptor;
 import kiber.bezbednjaci.dto.auth.AuthRequest;
 import kiber.bezbednjaci.dto.auth.AuthResponse;
 import kiber.bezbednjaci.dto.myrealestates.RealEstateResponse;
 import lombok.RequiredArgsConstructor;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.TrustStrategy;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.SSLContext;
 import javax.validation.Valid;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -28,54 +20,20 @@ import java.util.Objects;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
+    private final RestTemplate restTemplate;
+
     @PostMapping("/authenticate")
     public ResponseEntity<AuthResponse> authenticate(@Valid @RequestBody AuthRequest authRequest) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(csf)
-                .build();
-        HttpComponentsClientHttpRequestFactory requestFactory =
-                new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
         return restTemplate.postForEntity("https://localhost:8082/api/auth/login", authRequest, AuthResponse.class);
     }
 
     @PutMapping("/logout")
     public void logout() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(csf)
-                .build();
-        HttpComponentsClientHttpRequestFactory requestFactory =
-                new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
         restTemplate.put("https://localhost:8082/api/auth/logout", Void.class);
     }
 
     @GetMapping("/my-real-estates")
     public List<RealEstateResponse> myRealEstates() throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException {
-        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
-        SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
-                .loadTrustMaterial(null, acceptingTrustStrategy)
-                .build();
-        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
-        CloseableHttpClient httpClient = HttpClients.custom()
-                .setSSLSocketFactory(csf)
-                .build();
-        HttpComponentsClientHttpRequestFactory requestFactory =
-                new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setHttpClient(httpClient);
-        RestTemplate restTemplate = new RestTemplate(requestFactory);
         var realEstates = restTemplate.getForObject("https://localhost:8082/api/users/my-real-estates", RealEstateResponse[].class);
         if (Objects.isNull(realEstates)) {
             return List.of();
