7dff75926f12a07c097eabeb2687727aea03c84f
njmarko <marko.njegomir@uns.ac.rs>
2022-06-29 14:11:04 +0200

    Penetration testing - Buffer overflow - Added new validation for ConfigurDeviceRequest fields.

--- admin-backend/src/main/java/com/example/demo/dto/ConfigureDeviceRequest.java
+++ admin-backend/src/main/java/com/example/demo/dto/ConfigureDeviceRequest.java
@@ -1,6 +1,7 @@
 package com.example.demo.dto;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
@@ -10,10 +11,12 @@ import javax.validation.constraints.Positive;
 @Data
 public class ConfigureDeviceRequest {
     @NotBlank(message = "Name is required.")
+    @Length(max = 100, message = "Name cannot be longer than 100 characters.")
     @Pattern(regexp = "^(?=[a-zA-Z0-9._]{4,20}$)(?!.*[_.]{2})[^_.].*[^_.]$")
     private String name;
 
     @NotBlank(message = "Regex filter is required.")
+    @Length(max = 200, message = "Name cannot be longer than 100 characters.")
     private String regexFilter;
 
     @NotNull(message = "Send rate is required.")
